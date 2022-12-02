f6bc7ab7ab05bd75d4def5b3ac32e1cc250c0622
Ibrahim-MohamedH <Ibrahim20.Mohamed@gmail.com>
2022-12-02 12:44:57 +0200

    pre0.0.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@ subHead = $(".subHead");
 
 $(window).scroll(()=>{
     let y = $(window).scrollTop();
-    if(y > subHead.height() + 2){
+    if(y > subHead.height()){
         subHead.hide()
         mainHead.addClass("fixed-top")
     }else{
@@ -14,3 +14,37 @@ $(window).scroll(()=>{
     }
 })
 
+$(document).ready(function(){
+    $('.sale').slick({
+        infinite: true,
+        speed: 300,
+        rtl: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+            }
+        }
+        ]
+    });
+});
